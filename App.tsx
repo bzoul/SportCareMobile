3d1f35060170c370bf8763bd0ec90839b83ae38b
Lionel Bonzoumet <lionel.bonzoumet@epitech.eu>
2021-07-17 19:32:54 +0200

    feat(sensor): add fake data generator

--- App.tsx
+++ App.tsx
@@ -1,35 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Login from './views/Login';
 import Register from './views/Register';
+import Sensor from './views/Sensor';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 class App extends React.Component {
   render() {
     return (
-      <NavigationContainer >
-        <Stack.Navigator 
+      <NavigationContainer>
+        <Stack.Navigator
           screenOptions={{
-            headerShown: false
-        }}>
-          <Stack.Screen
-            name="Login"
-            component={Login}
-          />
-          <Stack.Screen
-            name="Register"
-            component={Register}
-          />
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Sensor" component={Sensor} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
         </Stack.Navigator>
       </NavigationContainer>
     );
   }
 }
 
-
 export default App;
-
