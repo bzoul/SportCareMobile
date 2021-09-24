ccb97638753acac129283940542615022bdc0fe8
Basile Rolland <brolland@demainunautrejour.com>
2021-09-24 16:53:11 +0200

    ajout des la bottom nav bar et creation de la page profil

--- App.tsx
+++ App.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './views/Login';
 import Register from './views/Register';
+import Test from './views/Test';
+import Chat from './views/Chat';
+import Profil from './views/Profil';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 const Stack = createStackNavigator();
 
@@ -17,12 +21,24 @@ class App extends React.Component {
             headerShown: false
         }}>
           <Stack.Screen
+            name="Test"
+            component={Test}
+          />
+          <Stack.Screen
             name="Login"
             component={Login}
           />
           <Stack.Screen
             name="Register"
             component={Register}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+          />
+          <Stack.Screen
+            name="Profil"
+            component={Profil}
           />
         </Stack.Navigator>
       </NavigationContainer>
---
+++ views/Chat.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Text, ScrollView, View , StyleSheet } from 'react-native'
+import BottomBar from '../components/blocs/Bottombar';
+
+export default class Test extends React.Component {
+    render() {
+        return (
+            <View>
+            <ScrollView style={styles.main_container}>
+
+                    
+            </ScrollView>
+            <View><BottomBar navigation={this.props.navigation}/></View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create ({
+    main_container: {
+        width:"100%",
+        backgroundColor: "blue",
+        height:"100%"
+    },
+    
+
+})
+
